e81a50c8759c34188f803b8c5e3e6061c9116234
Nightmayr <umayr23@gmail.com>
2019-02-24 15:19:33 +0000

    updated constants

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,15 @@
+// Local
+// export const BaseURL = "http://localhost:8081"
+// export const CreateAccountPageLink = "/CreateAccount"
+// export const ManageSessionPageLink = "/ManageSession"
+// export const AccCreate = "/accounts/createAccount"
+// export const ChangeBool = "/accounts/changeBool"
+// export const PathToGetAccount = "/accounts/getAccountById/{id}"
+// export const getAccounts = "/getAccounts"
+// export const accounts = "/accounts"
+// export const updateAccount = "/updateAccountById/{id}"
+
+// Docker
 export const BaseURL = "http://account:8081"
 export const CreateAccountPageLink = "/CreateAccount"
 export const ManageSessionPageLink = "/ManageSession"
